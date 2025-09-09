5ba9b21d0f8f74c41959b733b6a19eac0093cd1f
caiobarbosa7 <caio.bezerra@iesb.edu.br>
2025-09-08 21:24:13 -0300

    Update thread1.py

--- thread1.py
+++ thread1.py
@@ -1,27 +1,28 @@
 # importação de biblioteca
-import threading
-import time
+import threading # permite criar e controlar as threads
+import time # criar as pausas
 
 # estrutura da Thread
 def estrutura(nome, inicio, fim): # def é quando vai fazer uma estrutura, como o function no js
-    for i in range(inicio, fim +1): # .tofixed() é para limitar os números, como por exp. número infinito
+    for i in range(inicio, fim +1): 
         print(f"{nome} -> {i}")
 
     # pausa entre as durações de contagem
-    time.sleep(1) 
+    time.sleep(0.5) # pausa de 0.5 segundos entre cada número impresso
 
-# criar thread
-thread1 = threading.Thread(target=estrutura, args=("Thread-1", 1, 10))
-thread2 = threading.Thread(target=estrutura, args=("Thread-2", 50, 60)) # args é argumento
+# criação das threads com argumentos
+thread1 = threading.Thread(target=estrutura, args=("Thread-1", 1, 10)) # importa os números da thread1
+thread2 = threading.Thread(target=estrutura, args=("Thread-2", 50, 60)) # args é argumento # importa os números da thread2
 
-# rodar
+# inicia a execução das threads
 thread1.start()
 thread2.start()
 
-# estado de espera
+# estado de espera, aguarda as threads terminarem antes de continuar
 thread1.join()
 thread2.join()
 
-print("Todas as threads foram finalizadas.")
+print("Todas as threads foram finalizadas.") # fechamento após as threads terminarem com uma mensagem final
+
 
 
